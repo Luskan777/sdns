b2a7a2c1938e1c2320312b4c2164fa73b2f9e81f
Lucas Silva <lucas@atplus.com.br>
2021-08-05 10:59:52 -0300

    Consulta Dominios

---
+++ sdns.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+## Script simples em ShellScript para obter os dados de um dominio ##
+	
+if [ $1 == "--help"  ]
+then 
+
+echo "sdns - Search DNS
+ Programa para consultar Dominios na internet
+
+ -f   Caso queira expecificar algum arquivo que tenha uma lista de dominios
+ "
+
+elif [ $1 == "-f" ]
+ then
+
+    echo "Atenção, este comando adicionará um um diretório $PWD/dominios.d "
+
+    mkdir -p $PWD/dominios.d 2> /dev/null
+
+    for i in $(cat $2)  ; 
+    do nslookup $i | egrep "Name|Address"  | awk   '{print $1 " => " $2 }'| tail -n 2 > $PWD/dominios.d/$i ; 
+    dig $i mx +short | awk '{print  "MX => " $2 }' >> $PWD/dominios.d/$i ;
+    whois $1 | grep Registrar: | head -n1 | sed 's/Registrar/Registro/' >> $PWD/dominios.d/$i ;
+    whois $i | egrep "tech-c|nserver" | sed 's/tech-c/ID-Tecnico/' >> $PWD/dominios.d/$i ; 
+    whois $1 | grep 'Name Server' | head -n 3 >> $PWD/dominios.d/$i ;
+    whois $i | grep status >> $PWD/dominios.d/$i ;	 done
+
+    echo "As consultas dos dominios estão em '$PWD'/dominios.d "
+
+
+else
+
+dig $1 +short | awk '{print "Address => " $1}' 
+
+dig $1 mx +short | awk '{print "MX => " $2 }'
+
+whois $1 | grep Registrar: | head -n1 | sed 's/Registrar/Registro/'
+
+whois $1 | egrep "tech-c|nserver" | sed 's/tech-c/ID-Tecnico/' | sed 's/nserver/nameserver/'
+
+whois $1 | grep status:
+
+whois $1 | grep 'Name Server' | head -n 3
+
+fi
